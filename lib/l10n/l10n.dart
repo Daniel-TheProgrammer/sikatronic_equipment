9651f1790820c6581dcec59cbd8a09917a63be7d
Messi002 <rostandmessi2@gmail.com>
2022-12-22 00:09:13 +0100

    coding for fun

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class L10n {
+  static final all = [
+    //English - French - Spanish - Portugeues - German
+    const Locale('en'),
+    const Locale('fr'),
+    const Locale('es'),
+    const Locale('pt'),
+    const Locale('de'),
+  ];
+}
