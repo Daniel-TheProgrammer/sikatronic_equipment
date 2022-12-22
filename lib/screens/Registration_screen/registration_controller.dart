2e37b8bd64291d5a1d78bda7b71e10ccee0f76ef
Daniel-TheProgrammer <njid18753@gmail.com>
2022-12-22 23:17:03 +0100

    linking to firebase

--- lib/screens/Registration_screen/registration_controller.dart
+++ lib/screens/Registration_screen/registration_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RegistrationScreenController extends GetxController {
   var countryName = 'Country'.obs;
@@ -12,5 +13,30 @@ class RegistrationScreenController extends GetxController {
 
   getPhoneInit(String phoneNum) {
     phoneInit.value = phoneNum;
+  }
+
+  Future<void> addSellers(
+      {String? id,
+      String? name,
+      String? lastName,
+      String? email,
+      String? companyName,
+      String? country,
+      String? phone}) async {
+    CollectionReference users =
+        FirebaseFirestore.instance.collection('sellers');
+    return await users
+        .doc(id)
+        .set({
+          'sid': id,
+          'name': name,
+          'lastname': lastName,
+          'company': companyName,
+          'email': email,
+          'country': country,
+          'phone': phone,
+        })
+        .then((value) => print("Seller Added"))
+        .catchError((error) => print("Failed to add user: $error"));
   }
 }
