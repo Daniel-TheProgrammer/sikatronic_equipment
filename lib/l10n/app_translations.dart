8c3da89157b5d0ae38a95e9837436c0a524907d7
Messi002 <rostandmessi2@gmail.com>
2022-12-22 02:34:01 +0100

    coded this for fun

---
+++ lib/l10n/app_translations.dart
@@ -0,0 +1,45 @@
+
+
+import 'package:get/get.dart';
+
+///This is the translation core...
+class AppTranslations extends Translations{
+  @override
+
+  Map<String, Map<String, String>> get keys =>{
+    //The translation languages are English - French - German - Portuguese - Spanish
+
+  ///Add key-value pair like so e.g.
+  ///'title' : 'Easy translation translations',
+  ///'appbar1' : 'just testing',
+  ///where it is the title & appbar1 we are going to use not the actual 
+  ///english sentence
+      
+
+  //English
+    'en_US' : {
+ 
+    },
+
+  //French
+    'fr_FR' : {
+
+    },
+
+  //German
+    'de_DE' : {
+
+    },
+
+  //Portuguese
+    'pt_PT' : {
+
+    },
+
+  //Spanish
+    '	es_ES' : {
+
+    }
+  };
+
+}
